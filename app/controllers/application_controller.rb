9206a99ebc9cd6166ddd9875753e62f20f5968f7
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-14 14:51:51 -0600

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{ @article.id }"
   end
 
 
